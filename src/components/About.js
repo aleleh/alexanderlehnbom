9c5260ceabca297d9acd0ffa9b789ef2b8ebe3ae
aleleh <alexander.lehnbom@gmail.com>
2023-11-26 13:12:15 -0500

    new

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import { BiLogoReact } from "react-icons/bi";
 const About = () => {
   return (
     <Container className="my-5 center">
-    <h3>hi, i'm jonny 👋</h3>
+    <h3>hi, i'm alex 👋</h3>
       <p>
       I'm a tech support specialist and developer with a passion for running. I have experience in building applications with <Button href='https://nodejs.org/en/' variant="link" className='node-button' target="_blank">
           <BiLogoNodejs />Node.js
